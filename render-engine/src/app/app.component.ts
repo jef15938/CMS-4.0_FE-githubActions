8deaa8781a1def0de1a0c4e8f3480a323e3b0677
Bryant Wu <davidzxc12@gmail.com>
2020-04-20 14:18:11 +0800

    init git

---
+++ render-engine/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rdr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'render-engine';
+}
