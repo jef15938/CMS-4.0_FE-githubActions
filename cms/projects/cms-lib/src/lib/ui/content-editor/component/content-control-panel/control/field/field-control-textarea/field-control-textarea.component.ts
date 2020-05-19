d11494248512ec17968e7a76b3db4ee79e083fa3
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-05-19 15:09:28 +0800

    text-area限制行數(press enter/paste text)

--- cms/projects/cms-lib/src/lib/ui/content-editor/component/content-control-panel/control/field/field-control-textarea/field-control-textarea.component.ts
+++ cms/projects/cms-lib/src/lib/ui/content-editor/component/content-control-panel/control/field/field-control-textarea/field-control-textarea.component.ts
@@ -8,7 +8,32 @@ import { ContentControlBase } from '../../_base';
 })
 export class FieldControlTextareaComponent extends ContentControlBase implements OnInit {
 
+  readonly maxNewLine = 10;
   ngOnInit(): void {
+  }
+
+  onEnterDown(ev, value: string) {
+    const lines = (value || '').split(/\r*\n/);
+    if (lines.length >= this.maxNewLine) {
+      ev.stopPropagation();
+      ev.preventDefault();
+      alert(`行數最多允許${this.maxNewLine}行`);
+    }
+  }
+
+  onPaste(ev: ClipboardEvent) {
+    setTimeout(() => {
+      const lines = ((this.selected.fieldInfo.fieldVal || '') as string).split(/\r*\n/);
+      if (lines.length > this.maxNewLine) {
+        ev.stopPropagation();
+        ev.preventDefault();
+        alert(`行數最多允許${this.maxNewLine}行，多餘行數將被合併至最後一行`);
+        const overLimitLines = lines.splice(this.maxNewLine - 1, lines.length - this.maxNewLine + 1);
+        lines.push(overLimitLines.join(''));
+        this.selected.fieldInfo.fieldVal = lines.join("\n");
+      }
+    }, 0);
+    this.change.emit();
   }
 
 }
