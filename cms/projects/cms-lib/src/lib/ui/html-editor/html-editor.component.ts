87f3cad22cc8a59dc4fc2585ba80ff224d01060e
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-05-20 16:59:48 +0800

    微調

--- cms/projects/cms-lib/src/lib/ui/html-editor/html-editor.component.ts
+++ cms/projects/cms-lib/src/lib/ui/html-editor/html-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { map, takeUntil, finalize, concatAll, tap } from 'rxjs/operators';
 
@@ -28,8 +28,9 @@ let _this;
   templateUrl: './html-editor.component.html',
   styleUrls: ['./html-editor.component.scss']
 })
-export class HtmlEditorComponent implements OnInit {
+export class HtmlEditorComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('editorBlock') editorBlock: ElementRef<HTMLDivElement>;
   @Input() content = '';
 
   private imageSrc = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
@@ -54,9 +55,13 @@ export class HtmlEditorComponent implements OnInit {
   constructor(
     private _changeDetectorRef: ChangeDetectorRef
   ) { }
-
+  
   ngOnInit() {
     _this = this;
+  }
+
+  ngAfterViewInit(): void {
+    this.editorBlock.nativeElement.focus();
   }
 
   fontStyle(style: string) {
