35fc99b0c3f9efc923c2bb9545fbb1f87f0b4775
Sherman Shen <smilesheng19@gmail.com>
2020-06-08 22:37:11 +0800

    修正異常行為

--- cms/projects/cms-lib/src/lib/ui/html-editor/service/html-element-controller/table/table-controller.ts
+++ cms/projects/cms-lib/src/lib/ui/html-editor/service/html-element-controller/table/table-controller.ts
@@ -1,5 +1,5 @@
 import { HtmlEditorElementController } from './../_base';
-import { fromEvent, Subscription, merge, Observable } from 'rxjs';
+import { fromEvent, Subscription, merge, Observable, of, concat } from 'rxjs';
 import { IHtmlEditorContextMenuItem } from '../../../html-editor.interface';
 import { DeleteRow } from './actions/delete-row';
 import { AddRow } from './actions/add-row';
@@ -16,22 +16,50 @@ export class HtmlEditorTableController extends HtmlEditorElementController<HTMLT
 
   private _selectCellSubscription: Subscription;
 
+  protected onAddToEditor(): void {
+    this.contextMenuItems = [
+      { text: '上方列', icon: 'add', action: new AddRow(this.context, this, 'before') },
+      { text: '下方列', icon: 'add', action: new AddRow(this.context, this, 'after') },
+      { text: '刪除列', icon: 'delete', action: new DeleteRow(this.context, this) },
+      { text: '刪除欄', icon: 'delete', action: new DeleteCol(this.context, this) },
+    ];
+    const cols = this.el.querySelectorAll('tr')[0].querySelectorAll('td').length;
+    this._tableSetting = { cols };
+    this._subscribeEvents();
+    this._subscribeCellSelection();
+  }
+
+  protected onRemovedFromEditor(): void {
+    this._subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
+    this._subscriptions = [];
+    this.contextMenuItems = undefined;
+    this._unsubscribeCellSelection();
+  }
+
   getSetting(): ITableSetting {
     return this._tableSetting;
   }
 
-  private _evPreventDefaultAndStopPropagation = (ev: MouseEvent) => {
-    ev.preventDefault();
-    ev.stopPropagation();
+  private _subscribeEvents() {
+    const selectionchange$ = fromEvent(document, 'selectionchange').subscribe(_ => {
+      if (!this.context.isSelectionInsideEditorContainer) { return; }
+
+      const range = this.context.simpleWysiwygService.getRange();
+
+      if (this.el.contains(range?.startContainer)) {
+        this._onSelected();
+      } else {
+        this._onUnselected();
+      }
+    });
+    this._subscriptions.push(selectionchange$);
   }
 
-  private _subscribeCellSelection() {
-    console.warn(1);
-    try {
-      this._selectCellSubscription?.unsubscribe();
-    } catch (error) {
 
-    }
+  private _subscribeCellSelection() {
+    this._selectCellSubscription?.unsubscribe();
 
     const evPreventDefaultAndStopPropagation = (source: Observable<MouseEvent>) => new Observable<MouseEvent>(observer => {
       return source.subscribe({
@@ -95,14 +123,14 @@ export class HtmlEditorTableController extends HtmlEditorElementController<HTMLT
       });
     };
 
-    const tds = table.querySelectorAll('td');
+    const tds = Array.from(table.querySelectorAll('td')) as HTMLElement[];
 
     const mousedown$: Observable<MouseEvent> = merge(
-      ...Array.from(tds).map(td => fromEvent(td, 'mousedown'))
+      ...tds.map(td => fromEvent(td, 'mousedown'))
     ).pipe(evPreventDefaultAndStopPropagation);
 
     const mouseover$: Observable<MouseEvent> = merge(
-      ...Array.from(tds).map(td => fromEvent(td, 'mouseover'))
+      ...tds.map(td => fromEvent(td, 'mouseover'))
     ).pipe(evPreventDefaultAndStopPropagation);
 
     const mouseup$ = fromEvent<MouseEvent>(document, 'mouseup').pipe(evPreventDefaultAndStopPropagation);
@@ -114,6 +142,8 @@ export class HtmlEditorTableController extends HtmlEditorElementController<HTMLT
         disableDocumentMoveEvent = fromEvent<MouseEvent>(document, 'mousemove').pipe(evPreventDefaultAndStopPropagation).subscribe();
       }),
       switchMap(start => {
+        if (start.button === 2) { return of(undefined); } // right click
+
         // console.warn('start = ', start);
         removeAllSelected();
 
@@ -143,41 +173,10 @@ export class HtmlEditorTableController extends HtmlEditorElementController<HTMLT
     this._selectCellSubscription = drag$.subscribe();
   }
 
-  protected onAddToEditor(): void {
-    this.contextMenuItems = [
-      { text: '上方列', icon: 'add', action: new AddRow(this.context, this, 'before') },
-      { text: '下方列', icon: 'add', action: new AddRow(this.context, this, 'after') },
-      { text: '刪除列', icon: 'delete', action: new DeleteRow(this.context, this) },
-      { text: '刪除欄', icon: 'delete', action: new DeleteCol(this.context, this) },
-    ];
-    const cols = this.el.querySelectorAll('tr')[0].querySelectorAll('td').length;
-    this._tableSetting = { cols };
-    this._subscribeEvents();
-    this._subscribeCellSelection();
-  }
-
-  protected onRemovedFromEditor(): void {
-    this._subscriptions.forEach(subscription => {
-      subscription.unsubscribe();
-    });
-    this._subscriptions = [];
-    this.contextMenuItems = undefined;
+  private _unsubscribeCellSelection() {
     this._selectCellSubscription?.unsubscribe();
-  }
-
-  private _subscribeEvents() {
-    const selectionchange$ = fromEvent(document, 'selectionchange').subscribe(_ => {
-      if (!this.context.isSelectionInsideEditorContainer) { return; }
-
-      const range = this.context.simpleWysiwygService.getRange();
-
-      if (this.el.contains(range?.startContainer)) {
-        this._onSelected();
-      } else {
-        this._onUnselected();
-      }
-    });
-    this._subscriptions.push(selectionchange$);
+    const tds = Array.from(this.el.querySelectorAll('.selected')) as HTMLElement[];
+    tds.forEach(td => td.classList.remove('selected'));
   }
 
   private _onSelected(): void {
@@ -187,11 +186,12 @@ export class HtmlEditorTableController extends HtmlEditorElementController<HTMLT
 
   private _onUnselected(): void {
     this.el.style.removeProperty('outline');
+    this._unsubscribeCellSelection();
+  }
 
-    const tds = Array.from(this.el.querySelectorAll('.selected'));
-    tds.forEach(td => {
-      (td as HTMLElement).classList.remove('.selected');
-    })
+  private _evPreventDefaultAndStopPropagation = (ev: MouseEvent) => {
+    ev.preventDefault();
+    ev.stopPropagation();
   }
 
 }
