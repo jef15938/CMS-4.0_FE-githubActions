822de51af65cebab49989ea2929b27d96287add4
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-07-16 10:21:10 +0800

    render 調整 SliderComponent。目前放在 TabComponent中時功能異常

--- render-engine/projects/render/src/lib/global/component/slider/slider.component.ts
+++ render-engine/projects/render/src/lib/global/component/slider/slider.component.ts
@@ -13,42 +13,25 @@ import { MOCK_SOURCE_DATA } from './slider.mock';
   styleUrls: ['./slider.component.scss'],
 })
 export class SliderComponent extends DataSourceTemplateBaseComponent<SliderData> implements OnInit {
-  public show = true;
 
-  public slides = [
-    'First slide',
-    'Second slide',
-    'Third slide',
-    'Fourth slide',
-    'Fifth slide',
-    'Sixth slide'
-  ];
+  disabled = false;
 
-  public type = 'component';
-
-  public disabled = false;
-
-  public config: SwiperConfigInterface = {
-    a11y: true,
+  config: SwiperConfigInterface = {
+    updateOnWindowResize: true,
     direction: 'horizontal',
+    grabCursor: true,
+    loop: true,
     slidesPerView: 1,
+    a11y: true,
     keyboard: true,
-    mousewheel: true,
     scrollbar: false,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      bulletElement: 'span',
+      clickable: true,
+    },
     navigation: true,
-    pagination: false
-  };
-
-  private scrollbar: SwiperScrollbarInterface = {
-    el: '.swiper-scrollbar',
-    hide: false,
-    draggable: true
-  };
-
-  private pagination: SwiperPaginationInterface = {
-    el: '.swiper-pagination',
-    clickable: true,
-    hideOnClick: false
   };
 
   @ViewChild(SwiperComponent, { static: false }) componentRef?: SwiperComponent;
@@ -60,64 +43,11 @@ export class SliderComponent extends DataSourceTemplateBaseComponent<SliderData>
     super(injector, MOCK_SOURCE_DATA);
   }
 
-  public toggleType(): void {
-    this.type = (this.type === 'component') ? 'directive' : 'component';
-  }
-
-  public toggleDisabled(): void {
-    this.disabled = !this.disabled;
-  }
-
-  public toggleDirection(): void {
-    this.config.direction = (this.config.direction === 'horizontal') ? 'vertical' : 'horizontal';
-  }
-
-  public toggleSlidesPerView(): void {
-    if (this.config.slidesPerView !== 1) {
-      this.config.slidesPerView = 1;
-    } else {
-      this.config.slidesPerView = 2;
-    }
-  }
-
-  public toggleOverlayControls(): void {
-    if (this.config.navigation) {
-      this.config.scrollbar = false;
-      this.config.navigation = false;
-
-      this.config.pagination = this.pagination;
-    } else if (this.config.pagination) {
-      this.config.navigation = false;
-      this.config.pagination = false;
-
-      this.config.scrollbar = this.scrollbar;
-    } else {
-      this.config.scrollbar = false;
-      this.config.pagination = false;
-
-      this.config.navigation = true;
-    }
-
-    if (this.type === 'directive' && this.directiveRef) {
-      this.directiveRef.setIndex(0);
-    } else if (this.type === 'component' && this.componentRef && this.componentRef.directiveRef) {
-      this.componentRef.directiveRef.setIndex(0);
-    }
-  }
-
-  public toggleKeyboardControl(): void {
-    this.config.keyboard = !this.config.keyboard;
-  }
-
-  public toggleMouseWheelControl(): void {
-    this.config.mousewheel = !this.config.mousewheel;
-  }
-
-  public onIndexChange(index: number): void {
+  onIndexChange(index: number): void {
     console.log('Swiper index: ', index);
   }
 
-  public onSwiperEvent(event: string): void {
+  onSwiperEvent(event: string): void {
     console.log('Swiper event: ', event);
   }
 }
--- render-engine/projects/render/src/lib/global/component/slider/slider.mock.ts
+++ render-engine/projects/render/src/lib/global/component/slider/slider.mock.ts
@@ -4,22 +4,22 @@ export const MOCK_SOURCE_DATA: SliderData[] = [
   {
     id: '1',
     title: '2020「因為愛舞所聚ZUMBA ®」公益活動',
-    picture: 'https://www.transglobe.com.tw/transglobe_resource/leap_do/slider_picture/1592546903343/全球影音_ZUMBA宣傳影片.jpg',
-    url: 'https://youtu.be/6HarS8OaoOA',
+    picture: 'https://www.transglobe.com.tw/transglobe_resource/leap_do/slider_picture/1592278286507/4.官網大BN_1920x592_zumba副董款.jpg',
+    url: 'https://www.eventpal.com.tw/FOAS/actions/ActivityIndex.action?showTabContent&seqno=45b00272-5ed2-4938-afcf-566ad511075e',
     description: '全球人壽「因為愛舞所聚ZUMBA ®」公益活動，與全球人壽 X八三夭 ZUMBA® for Love展活力獻愛心！部分報名費捐助忠義基金會做為失依兒少助養金。讓我們一起因為愛，舞所聚！'
   },
   {
     id: '2',
     title: 'Mother Earth：全球挺全球',
-    picture: 'https://www.transglobe.com.tw/transglobe_resource/leap_do/slider_picture/1588834848704/全球挺全球.jpg',
-    url: 'https://www.youtube.com/watch?v=hTSOGREE1jA',
+    picture: 'https://www.transglobe.com.tw/transglobe_resource/leap_do/slider_picture/1586942979825/1920x592.png',
+    url: 'https://www.transglobe.com.tw/campaign/36winner/index.html',
     description: '2020年初始，一場未知疫情正悄悄蔓延，全球超過二分之一的國家受到影響，我們的母親─地球，給我們一次機會教育，讓全球不分國界全力抗疫，我們想藉由這個影片，向母親地球表達愛與致敬。'
   },
   {
     id: '3',
     title: '2020「因為愛舞所聚ZUMBA ®」公益活動',
-    picture: 'https://www.transglobe.com.tw/transglobe_resource/leap_do/slider_picture/1583122262170/庭聚.jpg',
-    url: 'https://bit.ly/2JW14lO',
+    picture: 'https://www.transglobe.com.tw/transglobe_resource/leap_do/slider_picture/1587979239786/官網大BN_1920x592_樂福貸.jpg',
+    url: 'https://www.transglobe.com.tw/campaign/love/index.html',
     description: '2020庭聚生活講堂隆重登場，3大系列主題+8位專家名人，場場安排知名藝人與專家學者，與您零距離分享豐富的生活知識與經歷。<br/>庭聚，為您打造週末聚會新主張！<br/>'
   },
 ];
