b328a157b00d566dd8c2e787809e8502957177b7
Sherman Shen <smilesheng19@gmail.com>
2020-07-21 16:44:33 +0800

    修正Lambda not supported

--- cms/projects/cms-lib/src/lib/global/util/form-validator.ts
+++ cms/projects/cms-lib/src/lib/global/util/form-validator.ts
@@ -2,7 +2,7 @@ import { FormGroup, ValidatorFn, ValidationErrors, AbstractControl } from '@angu
 
 export class CmsFormValidator {
 
-  static validDate = (abstractControl: AbstractControl) => {
+  static validDate(abstractControl: AbstractControl) {
     const { value, errors } = abstractControl;
     if (!value) { return null; }
     if (Object.prototype.toString.call(value) === '[object Date]' && !isNaN(value.getTime())) {
