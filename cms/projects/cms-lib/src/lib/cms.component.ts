559b0a6beef6997c3baaa3ebfb66454769517efc
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-08-11 16:19:45 +0800

    進入cms時進入第一個menu

--- cms/projects/cms-lib/src/lib/cms.component.ts
+++ cms/projects/cms-lib/src/lib/cms.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DialogFlowMessengerService } from './global/service';
 import { RippleScreen } from './global/util/cfx';
 import { AuthorizationService } from './global/api/service';
 import { LoginInfo } from './global/api/neuxAPI/bean/LoginInfo';
+import { MenuInfo } from './global/api/neuxAPI/bean/MenuInfo';
 
 @Component({
   selector: 'cms-cms',
@@ -20,6 +21,7 @@ export class CmsComponent implements OnInit, AfterViewInit {
   loginInfo: LoginInfo;
 
   constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     private authorizationService: AuthorizationService,
     private dialogFlowMessengerService: DialogFlowMessengerService,
@@ -36,10 +38,24 @@ export class CmsComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     // const menuContainerEle = this.menuContainer.nativeElement;
     // const rippleFx = new RippleScreen(menuContainerEle);
+    const currentUrl = this.router.url;
+    if (!currentUrl || currentUrl === '/') {
+      const firstMenuPath = this.getFirstMenuPath(this.cmsMenus);
+      if (firstMenuPath) {
+        this.router.navigateByUrl(firstMenuPath);
+      }
+    }
   }
 
   logout() {
     this.authorizationService.logout().subscribe();
+  }
+
+  getFirstMenuPath(menus: MenuInfo[], path = ''): string {
+    if (!menus?.length) { return path; }
+    const first = menus[0];
+    path += `/${first.func_id}`;
+    return this.getFirstMenuPath(first.children, path);
   }
 
 }
