5ce335ac5fc9aa08a7539c63a7cbdb2725ab0ee1
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-08-14 12:54:40 +0800

    調整RenderEffects import RenderService路徑

--- render-engine/projects/render/src/lib/global/store/effects/render.effects.ts
+++ render-engine/projects/render/src/lib/global/store/effects/render.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as RenderActions from '../actions/render.actions';
 import { switchMap, map, catchError } from 'rxjs/operators';
-import { RenderService } from '../../service';
+import { RenderService } from '../../service/render.service';
 import { of } from 'rxjs';
 
 
