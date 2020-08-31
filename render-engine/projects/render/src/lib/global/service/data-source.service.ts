b3ce00887a45fd693cab954d89f744a6c3c336e7
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-08-31 10:25:39 +0800

    @neux/render 版本變更，DataSourceService不用mock

--- render-engine/projects/render/src/lib/global/service/data-source.service.ts
+++ render-engine/projects/render/src/lib/global/service/data-source.service.ts
@@ -1,21 +1,9 @@
 import { Injectable } from '@angular/core';
 import { RestApiService } from '../api/neuxAPI/rest-api.service';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ListDataSourceDataResponse } from '../api/neuxAPI/bean/ListDataSourceDataResponse';
 import { ListDataSourceDataResponseModel } from '../api/data-model/models/list-data-source-data-response.model';
 import { ModelMapper } from '@neux/core';
-
-import * as download from './mock/download.json';
-import * as news from './mock/news.json';
-import * as qa from './mock/qa.json';
-import * as slider from './mock/slider.json';
-
-const mock = {
-  download,
-  news,
-  qa,
-  slider,
-};
 
 @Injectable({
   providedIn: 'root'
@@ -35,7 +23,6 @@ export class DataSourceService {
    * @memberof DataSourceService
    */
   getDataSourceByTypeIDAndId<TData>(typeID: string, id: string): Observable<ListDataSourceDataResponseModel<TData>> {
-    return of(mock[typeID]);
     return this.restAPIService.dispatchRestApi<ListDataSourceDataResponse>('GetDataSourceByTypeIDAndId', { typeID, id }).pipe(
       ModelMapper.rxMapModelTo(ListDataSourceDataResponseModel),
     );
