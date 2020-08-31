58415f60d6dc4fa101513f454f0219fcc4b93b51
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-08-31 16:01:09 +0800

    調整多選下拉關閉按鈕樣式

--- cms/projects/cms-lib/src/lib/global/directive/mat-select-multiple-close-btn/mat-select-multiple-close-btn.directive.ts
+++ cms/projects/cms-lib/src/lib/global/directive/mat-select-multiple-close-btn/mat-select-multiple-close-btn.directive.ts
@@ -18,11 +18,11 @@ export class MatSelectMultipleCloseBtnDirective implements OnInit, OnDestroy {
 
   ngOnInit() {
     const btn = document.createElement('button');
-    btn.innerText = 'X';
-    btn.setAttribute('class', 'btn btn-danger');
+    btn.innerText = '關閉';
+    btn.setAttribute('class', 'btn btn-cancel');
     btn.setAttribute(
       'style',
-      'position:absolute;right:0px;top:0;width:24px;height:24px;line-height:24px;vertical-align:middle;border:1px solid lightgray;border-radius:5px;'
+      'position:relative;left:0;bottom:0;width:100%;height:3em;line-height:3em;vertical-align:middle;border:1px solid lightgray;'
     );
     this.btn = btn;
 
@@ -43,6 +43,8 @@ export class MatSelectMultipleCloseBtnDirective implements OnInit, OnDestroy {
         const panelEl = this.select.panel.nativeElement as HTMLElement;
         const parent = panelEl.parentElement;
         parent.appendChild(this.btn);
+      } else {
+        this.unregisterBtn();
       }
     });
   }
@@ -50,6 +52,13 @@ export class MatSelectMultipleCloseBtnDirective implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.unsubscribe();
+    this.unregisterBtn();
+  }
+
+  private unregisterBtn() {
+    if (this.btn?.parentElement) {
+      this.btn.parentElement.removeChild(this.btn);
+    }
   }
 
 }
