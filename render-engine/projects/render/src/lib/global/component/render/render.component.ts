b14114e903187ff6803bb11714ca76c5fffc5031
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-09-21 19:31:06 +0800

    render engine 在 頁面header 加入 meta 相關

--- render-engine/projects/render/src/lib/global/component/render/render.component.ts
+++ render-engine/projects/render/src/lib/global/component/render/render.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PageData } from '../../types';
 import { LayoutInfo } from '../../interface/layout-info.interface';
 import { ContentInfoModel } from '../../api/data-model/models/content-info.model';
 import { SiteMapGetResponseModel } from '../../api/data-model/models/site-map-get-response.model';
 import { PageInfoGetResponseModel } from '../../api/data-model/models/page-info-get-response.model';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'rdr-render',
@@ -22,11 +23,14 @@ export class RenderComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
+    @Inject(DOCUMENT) private document: Document,
   ) { }
 
   ngOnInit(): void {
     const url = this.router.url;
     this.runtime = url.indexOf('/preview/') < 0;
+
+
 
     const { pageInfo, sitemap, contentInfo } = this.activatedRoute.snapshot.data.data as PageData;
 
@@ -45,6 +49,36 @@ export class RenderComponent implements OnInit {
       }
     }];
     console.warn('this.templates = ', this.templates);
+
+    this.createMetas(this.document, this.pageInfo);
+  }
+
+  private createMetas(document: Document, pageInfo: PageInfoGetResponseModel) {
+    const metaTitle = pageInfo.metaTitle || '';
+    const metaKeyword = pageInfo.metaKeyword || '';
+    const metaDescription = pageInfo.metaDescription || '';
+    const metaImage = pageInfo.metaImage || '';
+
+    const titles = document.head.querySelectorAll('title');
+    titles.forEach(title => title.innerText = metaTitle || '');
+
+    const metas = [
+      { key: 'title', value: metaTitle },
+      { key: 'keywords', value: metaKeyword },
+      { key: 'description', value: metaDescription },
+      { key: 'image', value: metaImage },
+    ];
+
+    metas.map(meta => {
+      const el = document.createElement('meta');
+      el.setAttribute('name', meta.key);
+      el.setAttribute('itemprop', meta.key);
+      el.setAttribute('property', `og:${meta.key}`);
+      el.setAttribute('content', meta.value);
+      return el;
+    }).forEach(metaEl => {
+      document.head.appendChild(metaEl);
+    });
   }
 
 }
