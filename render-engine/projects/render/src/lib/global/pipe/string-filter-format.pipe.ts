ada0f45e8247614f9be82be1c0390fb8f0f4b19d
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-09-30 14:05:40 +0800

    修正

--- render-engine/projects/render/src/lib/global/pipe/string-filter-format.pipe.ts
+++ render-engine/projects/render/src/lib/global/pipe/string-filter-format.pipe.ts
@@ -6,6 +6,7 @@ export class EllipsisPipe implements PipeTransform {
   constructor() { }
 
   transform(value: string, middleText: string, headLength: number, tailLength: number) {
+    if (!value) { return ''; }
     const headString = value.substring(0, headLength);
     const tailString = value.substring(value.length - tailLength - 1);
     if (headLength > value.length) {
