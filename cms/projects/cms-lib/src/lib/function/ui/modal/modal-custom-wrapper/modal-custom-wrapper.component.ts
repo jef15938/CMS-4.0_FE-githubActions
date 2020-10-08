721c776a86a846c1c89f758632d55c4e3db0e529
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-10-08 11:37:22 +0800

    cms model close btn position

--- cms/projects/cms-lib/src/lib/function/ui/modal/modal-custom-wrapper/modal-custom-wrapper.component.ts
+++ cms/projects/cms-lib/src/lib/function/ui/modal/modal-custom-wrapper/modal-custom-wrapper.component.ts
@@ -1,17 +1,20 @@
-import { Component, Inject, ViewChild, ComponentRef } from '@angular/core';
+import { Component, Inject, ViewChild, ComponentRef, AfterViewInit, ViewContainerRef, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+import { DynamicWrapperDirective } from '@neux/core';
 import { ModalOpenComponentConfig } from '../modal.interface';
 import { CustomModalBase } from '../base/custom-modal-base';
-import { DynamicWrapperDirective } from '@neux/core';
 
 @Component({
   selector: 'cms-modal-custom-wrapper',
   templateUrl: './modal-custom-wrapper.component.html',
   styleUrls: ['./modal-custom-wrapper.component.scss']
 })
-export class ModalCustomWrapperComponent {
+export class ModalCustomWrapperComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild(DynamicWrapperDirective) dynamicWrapperDirective: DynamicWrapperDirective<CustomModalBase<any, any>>;
+  @ViewChild('closeBtn', { read: ViewContainerRef }) closeBtn: ViewContainerRef;
 
   componentClass;
 
@@ -19,10 +22,69 @@ export class ModalCustomWrapperComponent {
 
   instanceTitle = '';
 
+  private mutationObserver: MutationObserver;
+
+  private destroy$ = new Subject();
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public config: ModalOpenComponentConfig<any, any>,
     public modalRef: MatDialogRef<ModalCustomWrapperComponent>,
-  ) { }
+    private viewContainerRef: ViewContainerRef,
+  ) {
+    modalRef.addPanelClass('cms-modal');
+  }
+
+  ngAfterViewInit(): void {
+    if (this.closeBtn) {
+      this.observeCloseBtn();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+    this.mutationObserver?.disconnect();
+  }
+
+  private observeCloseBtn() {
+    const selfEl = this.viewContainerRef.element.nativeElement as HTMLElement;
+    const parentEl = selfEl.parentElement;
+    const overlayPanelEl = parentEl.parentElement;
+    const btnEl = this.closeBtn.element.nativeElement as HTMLElement;
+    setTimeout(() => {
+      btnEl.style.transition = '0.5s';
+      btnEl.style.opacity = '1';
+      setTimeout(() => {
+        btnEl.style.transition = '';
+      }, 501);
+    }, 1);
+
+    this.calculateCloseBtnPosition(btnEl, overlayPanelEl);
+
+    const mutationObserver = new MutationObserver((records) => {
+      this.calculateCloseBtnPosition(btnEl, overlayPanelEl);
+    });
+
+    mutationObserver.observe(overlayPanelEl, {
+      attributeOldValue: true,
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true
+    });
+
+    this.mutationObserver = mutationObserver;
+
+    fromEvent(window, 'resize').pipe(
+      takeUntil(this.destroy$),
+      tap(_ => this.calculateCloseBtnPosition(btnEl, overlayPanelEl)),
+    ).subscribe();
+  }
+
+  private calculateCloseBtnPosition(btnEl: HTMLElement, overlayPanelEl: HTMLElement) {
+    btnEl.style.top = `${overlayPanelEl.offsetTop}px`;
+    btnEl.style.left = `${overlayPanelEl.offsetLeft + overlayPanelEl.offsetWidth}px`;
+  }
 
   onComponentLoad = (componentRef: ComponentRef<CustomModalBase<any, any>>) => {
     const instance = componentRef.instance;
