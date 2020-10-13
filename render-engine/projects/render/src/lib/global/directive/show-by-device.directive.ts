65fa50a147fbe8bf1ced24c6897847adb28e1a3e
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-10-13 17:09:54 +0800

    更新render

--- render-engine/projects/render/src/lib/global/directive/show-by-device.directive.ts
+++ render-engine/projects/render/src/lib/global/directive/show-by-device.directive.ts
@@ -1,14 +1,36 @@
-import { Directive, Input, TemplateRef, ViewContainerRef, Inject, PLATFORM_ID } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef, Inject, PLATFORM_ID, OnChanges, SimpleChanges } from '@angular/core';
 import { DeviceDetectionService } from '../service/device-detection.service';
 import { SiteMapInfoModel } from '../api/data-model/models/site-map-info.model';
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Directive({ selector: '[rdrShownByDevice]' })
-export class ShownByDeviceDirective {
+export class ShownByDeviceDirective implements OnChanges {
 
   @Input() set rdrShownByDevice(pageNode: SiteMapInfoModel) {
+    this.pageNode = pageNode;
+  }
+
+  @Input() set rdrShownByDeviceRuntime(runtime: boolean) {
+    this.runtime = runtime;
+  }
+
+  private pageNode: SiteMapInfoModel;
+  private runtime = false;
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    private deviceDetectionService: DeviceDetectionService,
+    @Inject(PLATFORM_ID) private platformId: any,
+    @Inject(DOCUMENT) private document: any,
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const runtime = this.runtime;
+    const node = this.pageNode;
+    console.warn({runtime, node});
     this.viewContainer.clear();
-    if (this.canRender(pageNode)) {
+    if (this.canRender(this.pageNode)) {
       this.viewContainer.createEmbeddedView(this.templateRef);
     } else {
       if (this.document) {
@@ -19,16 +41,9 @@ export class ShownByDeviceDirective {
     }
   }
 
-  constructor(
-    private templateRef: TemplateRef<any>,
-    private viewContainer: ViewContainerRef,
-    private deviceDetectionService: DeviceDetectionService,
-    @Inject(PLATFORM_ID) private platformId: any,
-    @Inject(DOCUMENT) private document: any,
-  ) { }
-
   private canRender(pageNode: SiteMapInfoModel) {
     if (!isPlatformBrowser(this.platformId)) { return true; }
+    if (!this.runtime) { return true; }
     if (!pageNode?.device) { return true; }
     const pageDevices = pageNode.device.split(',');
     const deviceType = this.deviceDetectionService.deviceType;
