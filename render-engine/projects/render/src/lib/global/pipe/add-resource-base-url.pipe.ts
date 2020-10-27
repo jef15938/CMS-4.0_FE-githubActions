6abb8c4fb1c0311323ef646ebc5c652f6d581c28
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-10-27 17:02:37 +0800

    程式更新

--- render-engine/projects/render/src/lib/global/pipe/add-resource-base-url.pipe.ts
+++ render-engine/projects/render/src/lib/global/pipe/add-resource-base-url.pipe.ts
@@ -1,6 +1,7 @@
-import { Pipe, PipeTransform, Inject } from '@angular/core';
+import { Pipe, PipeTransform, Inject, PLATFORM_ID } from '@angular/core';
 import { RENDER_ENVIROMENT_TOKEN } from '../injection-token/injection-token';
 import { RenderEnvironment } from '../interface/render-environment.interface';
+import { isPlatformBrowser } from '@angular/common';
 
 @Pipe({
   name: 'addResourceBaseUrl'
@@ -9,21 +10,26 @@ export class AddResourceBaseUrlPipe implements PipeTransform {
 
   constructor(
     @Inject(RENDER_ENVIROMENT_TOKEN) private enviroment: RenderEnvironment,
+    @Inject(PLATFORM_ID) private platformId: any,
   ) { }
 
   transform(htmlString: string, args?: any): string {
     if (!htmlString) { return htmlString; }
+
+    const isBrowser = isPlatformBrowser(this.platformId);
+    const resourceUrl = isBrowser ? this.enviroment.resourceBaseUrl : '';
+
     const div = document.createElement('div');
     div.innerHTML = htmlString;
 
     const galleryImgs = Array.from(div.querySelectorAll('img[gallery-id]')) as HTMLImageElement[];
     galleryImgs.forEach(galleryImg => {
-      galleryImg.src = `${this.enviroment.resourceBaseUrl}${galleryImg.getAttribute('src')}`;
+      galleryImg.src = `${resourceUrl}${galleryImg.getAttribute('src')}`;
     });
 
     const galleryFiles = Array.from(div.querySelectorAll('a.gallery-file')) as HTMLAnchorElement[];
     galleryFiles.forEach(galleryFile => {
-      galleryFile.href = `${this.enviroment.resourceBaseUrl}${galleryFile.getAttribute('href')}`;
+      galleryFile.href = `${resourceUrl}${galleryFile.getAttribute('href')}`;
     });
 
     return div.innerHTML || '';
