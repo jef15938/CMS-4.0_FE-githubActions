c6e2bf6ee6e9b109a29e0ac6f1907aee58cacc7f
JingTingZeng <neux.jingtingzeng@gmail.com>
2020-11-13 11:50:16 +0800

    新增快捷選單展開樣式與動畫

--- render-engine/projects/render/src/lib/global/component/public-component/fast-tool/fast-tool.component.ts
+++ render-engine/projects/render/src/lib/global/component/public-component/fast-tool/fast-tool.component.ts
@@ -1,19 +1,50 @@
-import { Component, OnInit, ViewEncapsulation, Injector } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, ViewEncapsulation, Injector, Input } from '@angular/core';
 import { CustomizeBaseDirective } from '../base-component';
+
+export interface FastToolData {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'rdr-fast-tool',
   templateUrl: './fast-tool.component.html',
   styleUrls: ['./fast-tool.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('animateSlide', [
+      state('false', style({
+        transform: 'translateX(192px)',
+      })),
+      state('true', style({
+        transform: 'translateX(0)'
+      })),
+      transition('false <=> true', animate('400ms cubic-bezier(.65, 0, .35, 1)'))
+    ])
+  ]
 })
 export class FastToolComponent extends CustomizeBaseDirective implements OnInit {
+
+  @Input() fastToolList: FastToolData[];
+
+  showFastTool = false;
 
   constructor(injector: Injector) {
     super(injector);
   }
 
   ngOnInit(): void {
+  }
+
+
+  /**
+   * 開關快捷選單
+   *
+   * @memberof FastToolComponent
+   */
+  toggleFastTool() {
+    this.showFastTool = !this.showFastTool;
   }
 
 }
