be10c07e8e8a3d1aba347e4dec32df08efcf4518
沈孝嶸 <neux.sherman.shen@gmail.com>
2020-11-18 10:29:03 +0800

    修正編輯器

--- cms/projects/cms-lib/src/lib/function/ui/html-editor/service/html-transformer.ts
+++ cms/projects/cms-lib/src/lib/function/ui/html-editor/service/html-transformer.ts
@@ -2,69 +2,96 @@ import { TABLE_CLASS_BASE_ROW } from '../const/html-editor-container.const';
 
 export class HtmlTransformer {
 
-  private readonly tagsToReplace = [
-    'style', 'script', 'applet', 'embed', 'noframes', 'noscript',
-    'o:p', 'em', 'b(?!ody)', 's(?!pan)(?!tyle)', 'u(?!l)', 'i',
-    'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+  private readonly tagsToRemove = [
+    'v:shape', 'v:shapetype', 'v:imagedata', 'img'
   ];
+
+  private readonly tagsToReplaceWithP = [
+    'style', 'script', 'applet', 'noframes', 'noscript', 'embed', 'o:p',
+    'em', 'b(?!ody)(?!r)', 's(?!pan)(?!tyle)', 'u(?!l)', 'i(?!mg)(?!frame)', 'a',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+  ].map(tag => `(${tag})`).join('|');
 
   private readonly attributesToClean = [
-    'href', 'lang', 'style', 'class', 'height', 'width', 'name', 'border',
-    'cellspacing', 'cellpadding', 'valign'
+    'href', 'lang', 'style', 'class', 'name', 'border',
+    'cellspacing', 'cellpadding', 'valign', 'unselectable', 'src', 'role',
+    'xml', 'xml:', 'xml:lang', 'paraid', 'paraeid', 'data-contrast', 'role', 'xml:', 'data-ccp-props'
   ];
 
-  /**
-   * 內容是否從 Microsoft Office
-   * @param content 檢查的內容
-   */
-  private isMso(content: string): boolean {
-    return /class=[",']?Mso|style=[",'][^"]*\bmso-|w:WordDocument/i.test(content);
-  }
+  transform(contentToTransform: string): string {
+    console.warn({ tagsToRemove: this.tagsToRemove, tagsToReplaceWithP: this.tagsToReplaceWithP, });
+    let content = contentToTransform;
 
-  private getTransformedMsoContent(msoContent: string) {
-    let content = msoContent;
-
-    // // 1. remove line breaks / Mso classes
-    // const stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
-    // content = content.replace(stringStripper, ' ');
-    // // 2. strip Word generated HTML comments
-    // const commentSripper = new RegExp('<!--(.*?)-->', 'g');
-    // content = content.replace(commentSripper, '');
-    // // 3. remove tags leave content if any
-    // let tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>', 'gi');
-    // content = content.replace(tagStripper, '');
-    // // 4. Remove everything in between and including tags '<style(.)style(.)>'
-    // const badTags = ['style', 'script', 'applet', 'embed', 'noframes', 'noscript'];
-    // for (const tag of badTags) {
-    //   tagStripper = new RegExp('<' + tag + '.*?' + tag + '(.*?)>', 'gi');
-    //   content = content.replace(tagStripper, '');
-    // }
-    // // 5. remove attributes ' style="..."'
-    // const badAttributes = ['style', 'start'];
-    // for (const attr of badAttributes) {
-    //   const attributeStripper = new RegExp(' ' + attr + '="(.*?)"', 'gi');
-    //   content = content.replace(attributeStripper, '');
-    // }
+    this.tagsToRemove.forEach(tag => {
+      content = content.replace(new RegExp(`<${tag} .*?>`, 'g'), '');
+      content = content.replace(new RegExp(`</${tag}>`, 'g'), '');
+    });
 
     content = content
       .replace(/<\!--[\s\S]*?-->/g, '')
       .replace(/<\!\[.*?\]>/g, '')
-      .replace(/<a/g, '<span').replace(/<\/a>/g, '</span>')
-      .replace(new RegExp(`<(${this.tagsToReplace.map(tag => `(${tag})`).join('|')})`, 'g'), '<span')
-      .replace(new RegExp(`</(${this.tagsToReplace.map(tag => `(${tag})`).join('|')})>`, 'g'), '</span>');
+      .replace(new RegExp(`<(${this.tagsToReplaceWithP})`, 'g'), '<span')
+      .replace(new RegExp(`</(${this.tagsToReplaceWithP})>`, 'g'), '</span>')
+      ;
 
     const html = document.createElement('html');
     html.innerHTML = content;
     const body = html.querySelector('body');
 
     const allElements = Array.from(body.getElementsByTagName('*'));
+
+    this.transformCmsTable(allElements, body);
+
     allElements.forEach(el => {
+      if (this.tagsToRemove.indexOf(el.tagName?.toLowerCase()) > -1) {
+        el.parentElement.removeChild(el);
+        return;
+      }
       this.attributesToClean.forEach(attr => {
         el.setAttribute(attr, '');
       });
     });
 
-    const tables = Array.from(body.querySelectorAll('table'));
+    this.tansformTable(allElements);
+
+
+    let innerHTML = body.innerHTML;
+
+    this.attributesToClean.forEach(attr => {
+      const regex = new RegExp(`${attr}=""`, 'g');
+      innerHTML = innerHTML.replace(regex, '');
+    });
+
+    innerHTML = innerHTML
+      .replace(/xml:/g, '')
+      .replace(/<p\s+><\/p>/g, '')
+      .replace(/<span\s+>/g, '').replace(/<\/span>/g, '')
+      ;
+
+    return innerHTML;
+  }
+
+  /** 避免從編輯器上複製再貼上的時候，處理 table 的階段異常，因此先將 neux-table 轉回成一般 table */
+  private transformCmsTable(elements: Element[], container: HTMLElement) {
+    const cmsTables = elements.filter(el => el.classList?.contains('neux-table'));
+    cmsTables.forEach(table => {
+      const tableID = table.id;
+      const tableWrap = container.querySelector(`[tableid=t${tableID}].neux-table-wrap`);
+      if (tableWrap) {
+        tableWrap.parentElement.insertBefore(table, tableWrap);
+        tableWrap.parentElement.removeChild(tableWrap);
+      }
+      const firstTBodyTr = table.querySelector('tbody > tr');
+      const tHeadTrs = Array.from(table.querySelectorAll('thead > tr'));
+      tHeadTrs.forEach(tr => {
+        tr.parentElement.removeChild(tr);
+      });
+      table.querySelector('thead').appendChild(firstTBodyTr);
+    });
+  }
+
+  private tansformTable(elements: Element[]) {
+    const tables = elements.filter(el => el.tagName?.toLowerCase() === 'table');
     tables.forEach(table => {
       const colgroups = table.querySelectorAll('colgroup');
       colgroups.forEach(cg => {
@@ -91,41 +118,5 @@ export class HtmlTransformer {
       const firstTr = tbody.firstElementChild;
       !!firstTr ? tbody.insertBefore(headerTr, firstTr) : tbody.appendChild(headerTr);
     });
-
-    let innerHTML = body.innerHTML;
-
-    this.attributesToClean.forEach(attr => {
-      const regex = new RegExp(`${attr}=""`, 'g');
-      innerHTML = innerHTML.replace(regex, '');
-    });
-
-    innerHTML = innerHTML
-      .replace(/<p\s+><\/p>/g, '')
-      .replace(/<span\s+>/g, '').replace(/<\/span>/g, '')
-      ;
-
-    return innerHTML;
-  }
-
-  transform(contentToTransform: string): string {
-    const isMso = this.isMso(contentToTransform);
-    if (isMso) { return this.getTransformedMsoContent(contentToTransform); }
-
-    const html = document.createElement('html');
-    html.innerHTML = contentToTransform;
-    const body = html.querySelector('body');
-
-    const tables = Array.from(body.querySelectorAll('table.neux-table'));
-    tables.forEach(table => {
-      const newId = `${new Date().getTime()}`;
-      const tableID = table.id;
-      const tableWrap = body.querySelector('[tableid].neux-table-wrap');
-      table.id = newId;
-      if (tableWrap) {
-        tableWrap.setAttribute('tableid', `t${newId}`);
-      }
-    });
-
-    return body.innerHTML;
   }
 }
